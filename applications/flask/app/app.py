e3935849b2ae1a0c8845849b3736ff26083da2b6
aleemahmad0508 <aleem.ahmad0508@gmail.com>
2026-08-17 08:44:15 +0500

    fix workflow

--- applications/flask/app/app.py
+++ applications/flask/app/app.py
@@ -45,7 +45,7 @@ FlaskInstrumentor().instrument_app(app)
 # -----------------------------
 @app.route("/")
 def home():
-    return "Welcome to flaskssss"
+    return "Welcome to flaskss"
 
 @app.route("/health")
 def health():
